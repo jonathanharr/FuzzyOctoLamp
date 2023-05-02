8ff01282042fff61235f51ff8f2ef1d579625afb
DieselWeazel <jonathan.harr92@gmail.com>
2023-05-02 17:29:51 +0200

    Some code cleanup

--- Jonathan.Harr.FuzzyOctoLamp/Service/SetLampService.cs
+++ Jonathan.Harr.FuzzyOctoLamp/Service/SetLampService.cs
@@ -12,8 +12,7 @@ using Newtonsoft.Json;
 namespace Jonathan.Harr.FuzzyOctoLamp.Service;
 
 public class SetLampService : ISetLampService
-{
-    // private static HttpClient _httpClient;
+{ 
     private readonly IHttpHueClient _hueClient;
     private static string _lightId;
     
@@ -25,15 +24,6 @@ public class SetLampService : ISetLampService
     
     public async Task<string> SetLampToColor(string xValue, string yValue)
     {
-        // TODO ersätt?
-        // var apiUser = "JkKLL75ApxQmtbqZdt79BeSYKE-519yZ7159q1Sk";
-        // var lightId = "10";
-
-        // TODO ersätt?
-        // var accessToken = "E1dkaQ17XNLIf774e0aSESNYC5A4";
-
-        // var apiUrl = $"https://api.meethue.com/bridge/{_apiUser}/lights/{_lightId}/state";
-        
         var culture = CultureInfo.InvariantCulture;
 
         if (!float.TryParse(xValue, NumberStyles.Float, culture, out var parsedXValue) ||
@@ -52,20 +42,5 @@ public class SetLampService : ISetLampService
         }
         
         return $"The color of the lamp with ID {_lightId} was successfully set to X={xValue}, Y={yValue}.";
-        // var body = new
-        // {
-        //     on = true,
-        //     xy = new[] { parsedXValue, parsedYValue },
-        //     bri = 100,
-        //     transitiontime = 0
-        // };
-        //
-        // var jsonBody = JsonConvert.SerializeObject(body);
-        // var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
-        // _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
-        //
-        // var response = await _httpClient.PutAsync(apiUrl, content);
-        //
-        // return response.IsSuccessStatusCode ? $"The color of the lamp with ID {_lightId} was successfully set to X={xValue}, Y={yValue}." : $"An error occurred while trying to set the lamp color: {response.ReasonPhrase}";
     }
 }
